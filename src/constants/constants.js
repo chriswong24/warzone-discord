e38c274379681bf6f771db3c93ced51c3ece5a39
Chris Wong <chriswong1224@gmail.com>
2020-12-31 16:09:28 -0500

    adding constants file

---
+++ src/constants/constants.js
@@ -0,0 +1,18 @@
+const loginInfo = {
+  username: 'username',
+  password: 'password',
+}
+
+const platforms = {
+  psn: 'psn',
+  steam: 'steam',
+  xbl: 'xbl',
+  battle: 'battle',
+  acti: 'acti',
+  uno: 'uno'
+}
+
+module.exports = {
+  loginInfo: loginInfo,
+  platforms: platforms,
+};
